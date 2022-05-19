19ccd1abb11c46aa0a60bb55a84a940128544dab
Usmanova555 <65666698+Usmanova555@users.noreply.github.com>
2022-05-19 15:29:24 +0300

    Add files via upload

---
+++ Lab15/Laboratory15/Teachers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_16
+{
+    class Teacher : Totality
+    {
+        string Subject;
+        public Teacher()
+        {
+
+        }
+        public Teacher(string name, string lastname, string subject) : base (name, lastname)
+        {
+            this.Subject = subject;
+        }
+        public override bool Proverka()
+        {
+            bool ff = false;
+            Console.WriteLine("Учитель проживает в Казани?");
+            string c = Console.ReadLine();
+
+            while (c.ToLower() != "да")
+            {
+                if (c.ToLower() == "нет")
+                    break;
+            }
+            if (c.ToLower() == "да")
+                ff = true;
+            else
+                ff = false;
+            return ff;
+        }
+        public string Subjectt()
+        {
+            Console.WriteLine("Введите название предмета");
+            Subject = Console.ReadLine();
+            return Subject.ToLower();
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Введите имя преподавателя");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию преподавателя");
+            lastname = Console.ReadLine();
+            Console.WriteLine($"Преподаватель {name} {lastname} преподаёт предмет - {Subjectt()}");
+        }
+    }
+}
---
+++ Lab15/Laboratory15/Totality.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_16
+{
+    abstract class Totality
+    {
+        protected string name;
+        protected string lastname;
+        string Name { get { return name; } set { name = value; } }
+        string Lastname { get { return lastname; } set { lastname = value; } }
+        public Totality()
+        {
+
+        }
+        abstract public bool Proverka();
+
+        public Totality (string name, string lastname)
+        {
+            this.Name = name;
+            this.Lastname = lastname;
+        }
+        virtual public bool Place()
+        {
+            bool place = false;
+            Console.WriteLine("Работает или учится в университете? Да/Нет");
+            string r = Console.ReadLine();
+
+            while (r.ToLower()!="да")
+            {
+                if (r.ToLower() == "нет")
+                    break;
+            }
+            if (r.ToLower() == "да")
+                place = true;
+            else place = false;
+            return place;
+        }
+        abstract public void Print();
+    } 
+}
